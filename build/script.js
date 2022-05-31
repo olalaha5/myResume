7a1bdba1b17c47bda69e68a56fc632912a288780
IlyaOl <81174461+olalaha5@users.noreply.github.com>
2022-05-31 16:13:57 +0300

    Add files via upload

--- build/script.js
+++ build/script.js
@@ -1,39 +1,85 @@
 "use strict";
 
+let block = document.querySelector('.container__wrapper')
 let btn = document.querySelectorAll('.nav__btn');
+let img = document.querySelectorAll('.nav__img');
 let elem = [document.querySelector('.about-me'), document.querySelector('.works'), document.querySelector('.contact-me')]
 let w = 0
-
-//функция изменения прозрачности
-function opacityChange(elem, from, to) {
-    let thisOpacity = from
-    let interval = setInterval(() => {
-        thisOpacity += (to - from) / 10
-        elem.style.opacity = thisOpacity
-    }, 10)
-    setTimeout(() => clearInterval(interval), 1000)
-}
+let stack = ['', '']
 
 //функция нужная для анимации
+let duration = 2000
+
 function display(i) {
     for (let item of elem) {
-        opacityChange(item, 1, 0)
+        item.style.opacity = "0"
     };
+    setTimeout(
+        () => {
+            if (stack[1] !== '') {
+                i = stack[1];
+                console.log(i)
+                stack[0] = stack[1];
+                stack[1] = '';
+            };
+        }, duration);
     setTimeout(
         () => {
             for (let item of elem) {
                 item.style.display = 'none'
             };
-            elem[i].style.display = 'block'
-            opacityChange(elem[i], 0, 1)
+            elem[i].style.display = "block"
+            block.style.height = elem[i].offsetHeight + 'px'
+            setTimeout(
+                () => {
+                    console.log(i)
+                    elem[i].style.opacity = "1";
+                    setTimeout(
+                        () => {
+                            if (stack[1] !== '') {
+                                stack[0] = stack[1];
+                                display(stack[1]);
+                                stack[1] = '';
+                            } else {
+                                stack[0] = ''
+                            }
+                        }, duration);
+                }
+                , 10);
         }
-        , 1000);
-}
+        , duration + 10);
+};
 
 //функция определяющая на какую кнопку нажали
 function click() {
     for (let i = 0; i < btn.length; i++) {
-        btn[i].onclick = () => display(i)
+        btn[i].onclick = () => {
+            if (stack[0] !== '') {
+                stack[1] = i
+            } else {
+                stack[0] = i
+                display(i);
+            }
+
+            positionTriangle(btn[i]);
+            for (let item of img) {
+                item.style.opacity = "0.5"
+            }
+            img[i].style.opacity = "1"
+        };
     };
 }
 click()
+
+
+function positionTriangle(element) {
+    let triangle = document.querySelector('.header__triangle');
+    // triangle.style.transform = 'rotate(0deg)';
+    triangle.style.left = `${element.offsetWidth / 2 + element.offsetLeft - 20}px`;
+    // triangle.style.top = '-69px';
+}
+
+positionTriangle(btn[0])
+block.style.height = elem[0].offsetHeight + 'px'
+img[0].style.opacity = "1"
+
--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,21 @@ const {
 
 
 
-const cssnano = require('gulp-cssnano');
+const csso = require('gulp-csso');
 // const changed = require('gulp-changed');
 const browsersync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 const clean = require('gulp-clean');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const pug = require('gulp-pug');
 const autoprefixer = require('gulp-autoprefixer');
 const shorthand = require('gulp-shorthand');
+const imageminJpegRecompress = require('imagemin-jpeg-recompress');
+const pngquant = require('imagemin-pngquant');
+const flatten = require('gulp-flatten');
+const imageResize = require('gulp-image-resize');
+const rename = require("gulp-rename");
 
 
 
@@ -34,28 +39,53 @@ function fullCssCompilation() {
     return src('./src/blocks/**/*.sass')
         .pipe(concat('style.sass'))
         .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(shorthand())
-        .pipe(cssnano())
+        .pipe(autoprefixer({
+            grid: true
+        }))
+        .pipe(shorthand({
+        }))
+        .pipe(csso())
         .pipe(dest('./build'))
         .pipe(browsersync.stream());
 }
 
 
 
-function img() {
-    return src('./src/**/*.png')
-        .pipe(imagemin())
-        .pipe(dest('./build/img'));
+// function img() {
+//     return src('./src/**/*.png') //Выберем наши картинки
+//         .pipe(flatten({ includeParents: 0 }))
+//         .pipe(imagemin([
+//             pngquant({
+//                 quality: [0.1, 0.2]
+//             })
+//         ], {
+//             verbose: true
+//         }))
+//         .pipe(dest('./build/img'));
+// }
+
+
+
+function img(cb) {
+    [300, 800, 1000, 2000].forEach(function (size) {
+        src('src/blocks/**/*.{jpg,jpeg,png}')
+            .pipe(flatten({ includeParents: 0 }))
+            .pipe(imagemin([
+                pngquant({
+                    quality: [0.7, 0.8]
+                })
+            ], {
+                verbose: true
+            }))
+            .pipe(dest('./build/img'))
+            .pipe(imageResize({ width: size }))
+            .pipe(rename(function (path) { path.basename = `${path.basename} ${size}w`; }))
+            .pipe(dest('./build/img__resize'))
+    });
+    cb()
 }
 
 
-
-function html() {
-    return src('./src/*.html')
-        .pipe(dest('./build/'))
-        .pipe(browsersync.stream());
-}
 
 function fullHtmlCompilation() {
     return src('./src/pages/*.pug')
@@ -69,7 +99,7 @@ function fullHtmlCompilation() {
 function watchFiles() {
     watch('src/blocks/**/*.sass', fullCssCompilation);
     watch('./src/pages/*.pug', fullHtmlCompilation);
-    watch('./src/**/*.png', img);
+    watch('./src/blocks/**/*.png', img);
 }
 
 
@@ -85,3 +115,4 @@ function browserSync() {
 
 exports.watch = parallel(watchFiles, browserSync);
 exports.default = series(clear, parallel(fullHtmlCompilation, fullCssCompilation, img));
+exports.image = parallel(img);
